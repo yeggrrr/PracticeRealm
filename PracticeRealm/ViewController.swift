f2e0bf1f034e633b018e0ac0f84f707b5798cabb
yeggrrr <yjkim981103@gmail.com>
2024-07-18 23:57:09 +0900

    Initial Commit

---
+++ PracticeRealm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeRealm
+//
+//  Created by YJ on 7/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
