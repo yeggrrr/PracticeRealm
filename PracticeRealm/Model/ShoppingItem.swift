f0b6effd19ca02cf60c56e18e796a7fdaff1cc86
yeggrrr <yjkim981103@gmail.com>
2024-07-19 01:21:09 +0900

    [Feat] json 데이터 불러오기 및 tableView에 업데이트

--- PracticeRealm/Model/ShoppingItem.swift
+++ PracticeRealm/Model/ShoppingItem.swift
@@ -7,12 +7,18 @@
 
 import Foundation
 
-struct ShoppingList {
+struct ShoppingList: Decodable {
     let list: [ShoppingItem]
 }
 
-struct ShoppingItem {
+struct ShoppingItem: Decodable {
     let name: String
     let price: Int
     let remainingStock: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case name = "name"
+        case price = "price"
+        case remainingStock = "remaining_stock"
+    }
 }
--- PracticeRealm/ShoppingList/ShoppingListViewController.swift
+++ PracticeRealm/ShoppingList/ShoppingListViewController.swift
@@ -13,7 +13,7 @@ class ShoppingListViewController: UIViewController {
     private let shoppingListView = ShoppingListView()
     
     // MARK: Properties
-    
+    var shoppingList: [ShoppingItem] = []
     
     // MARK: View Life Cycle
     override func loadView() {
@@ -24,6 +24,7 @@ class ShoppingListViewController: UIViewController {
         super.viewDidLoad()
         
         configure()
+        getShoppingList()
     }
     
     func configure() {
@@ -33,6 +34,37 @@ class ShoppingListViewController: UIViewController {
     }
     
     // MARK: Functions
+    func load(fileName: String) -> Data? {
+        guard let fileLocation = Bundle.main.url(forResource: fileName, withExtension: "json") else { return nil }
+        
+        do {
+            let data = try Data(contentsOf: fileLocation)
+            return data
+        } catch {
+            return nil
+        }
+    }
+    
+    func getShoppingList() {
+        guard let data = load(fileName: "ShoppingData") else { return }
+        
+        do {
+            let decoder = JSONDecoder()
+            let result = try decoder.decode(ShoppingList.self, from: data)
+            shoppingList = result.list
+            shoppingListView.tableView.reloadData()
+        } catch {
+            shoppingList = []
+            showAlert(title: "⚠️", message: "데이터를 가자오는데 실패했습니다.\n다시 시도해주세요.")
+        }
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let closeAction = UIAlertAction(title: "닫기", style: .default)
+        alert.addAction(closeAction)
+        present(alert, animated: true)
+    }
     
     // MARK: Actions
 }
@@ -40,13 +72,14 @@ class ShoppingListViewController: UIViewController {
 // MARK: UITableViewDataSource
 extension ShoppingListViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return shoppingList.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: ShoppingListTableViewCell.identifier, for: indexPath) as? ShoppingListTableViewCell else { return UITableViewCell() }
-        cell.nameLabel.text = "name"
-        cell.priceLabel.text = "10000원"
+        let item = shoppingList[indexPath.row]
+        cell.nameLabel.text = "\(item.name) (남은수량: \(item.remainingStock)개)"
+        cell.priceLabel.text = item.price.formatted() + "원"
         return cell
     }
 }
