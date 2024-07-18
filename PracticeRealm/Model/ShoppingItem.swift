4def4e6a65541767b7b20b11936c85270196333a
yeggrrr <yjkim981103@gmail.com>
2024-07-19 00:54:00 +0900

    [Add] ShoppingItem 모델 추가

---
+++ PracticeRealm/Model/ShoppingItem.swift
@@ -0,0 +1,14 @@
+//
+//  ShoppingItem.swift
+//  PracticeRealm
+//
+//  Created by YJ on 7/19/24.
+//
+
+import Foundation
+
+struct ShoppingItem {
+    let name: String
+    let price: Int
+    let remainingStock: Int
+}
