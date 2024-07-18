993e3710d8d2bf17394970347b3fe8f29c56e917
yeggrrr <yjkim981103@gmail.com>
2024-07-19 00:22:46 +0900

    [Add] ViewRepresentable Protocol 추가

---
+++ PracticeRealm/ViewRepresentable.swift
@@ -0,0 +1,14 @@
+//
+//  ViewRepresentable.swift
+//  PracticeRealm
+//
+//  Created by YJ on 7/19/24.
+//
+
+import Foundation
+
+protocol ViewRepresentable {
+    func addSubviews()
+    func setConstraints()
+    func configure()
+}
